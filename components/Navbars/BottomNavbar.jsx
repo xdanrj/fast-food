5a5d832fa51acff6304357ee89e89bcb7419735e
xdanrj <xdanrj@gmail.com>
2024-06-27 16:59:54 -0300

    17h

--- components/Navbars/BottomNavbar.jsx
+++ components/Navbars/BottomNavbar.jsx
@@ -3,23 +3,23 @@ import { HomeIcon, CartIcon, GlassIcon } from "../icons";
 export default function BottomNavbar() {
   return (
     <div className="fixed bottom-0 w-full bg-neutral-800">
-      <ul className="flex list-none justify-around my-2">
+      <ul className="flex list-none justify-around">
         <li >
           <a href="#" className="flex flex-col items-center">
-            <HomeIcon className="size-6"/>
-            <p>Início</p>
+            <HomeIcon className="size-4"/>
+            <p className="text-sm">Início</p>
           </a>
         </li>
         <li >
           <a href="#" className="flex flex-col items-center">
-            <GlassIcon className="size-6" />
-            <p>Buscar</p>
+            <GlassIcon className="size-4" />
+            <p className="text-sm">Buscar</p>
           </a>
         </li>
         <li >
           <a href="#" className="flex flex-col items-center">
-            <CartIcon className="size-6"/>
-            <p>Carrinho</p>
+            <CartIcon className="size-4"/>
+            <p className="text-sm">Carrinho</p>
           </a>
         </li>
       </ul>
--- pages/product.jsx
+++ pages/product.jsx
@@ -1,10 +1,23 @@
-import {ArrowLeft} from "../components/icons"
+import { ArrowLeft } from "../components/icons"
 export default function Product() {
   return (
-    <>
-    <div className="flex">
-      <ArrowLeft className="size-12"/> <p>Nome do produto</p>
+    <div className="p-4">
+      <div className="flex relative justify-center ">
+        <ArrowLeft className="size-8 absolute left-0" />
+        <p className="text-2xl">Nome do produto</p>
+      </div>
+
+      <div className="my-3 mx-auto bg-green-900 rounded-xl w-52 h-52"></div>
+
+      <p className="text-center">Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Nullam sit amet tempus lectus.
+        Integer leo neque, pretium ac maximus et,
+        eleifend in leo.</p>
+      <div className="flex justify-center">
+
+        <button className="mr-2 p-1 text-xl rounded border-1 border-white">- 1 +</button>
+        <button> Adicionar R$19,99</button>
+      </div>
     </div>
-    </>
   )
 }
