88e581e3ed0b12bbe4a97efc318d313ad51c5b99
xdanrj <xdanrj@gmail.com>
2024-06-28 15:09:52 -0300

    15h10

--- components/icons.jsx
+++ components/icons.jsx
@@ -71,3 +71,14 @@ return (
 </>
 )
 }
+
+export function XIcon(props){
+return (
+<>
+<svg {...props}  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+</svg>
+
+</>
+)
+}
--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,12 +1,12 @@
-import { ArrowLeft, TrashIcon } from "../components/icons"
+import { ArrowLeft, TrashIcon, XIcon } from "../components/icons"
 
 const Orders = {
   products: [
-    { "name": "Hambúrguer de frango e cheddar", "quantity": 1, "price": 19.99 },
+    { "name": "Hambúrguer de frango e cheddar", "quantity": 1, "price": 7919.99 },
     { "name": "Sundae de chocolate com calda de morango", "quantity": 2, "price": 6.99 }
   ]
 }
-export default function History() {
+export default function Cart() {
   return (
     <div className="">
 
@@ -16,9 +16,25 @@ export default function History() {
         <TrashIcon className="size-8 absolute right-0" />
       </div>
 
-      <div className="mt-6 w-full h-16 border-white-500 border-t-1 border-b-1">
+      {Orders.products.map((item, idx) => (
+        <div key={idx} className=" flex p-2 items-center mt-6 w-full h-16 border-white-500 border-t-1 border-b-1">
+          <div className="flex text-lg w-16">
+            <p className="">-</p>
+            <p className="mx-2">{item.quantity}</p>
+            <p className="">+</p>
+          </div>
 
-      </div>
+          <p className="text-left text-md w-3/5 bg-blue-900">{item.name}</p>
+
+            <div className="flex ml-auto items-center mx-2 bg-green-500">
+              <p className="text-right">R${item.price}</p>
+              <XIcon className="size-5" />
+            </div>
+         
+        </div>
+      ))
+
+      }
     </div>
   )
 }
